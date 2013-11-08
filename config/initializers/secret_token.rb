aa59aee33e67f761448287e714b85114b70fdb27
LuciannaAlmeida <lucianna.th@gmail.com>
2013-11-08 16:52:41 -0200

    adding whole project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AliveFulfillmentApp::Application.config.secret_token = 'fc10f852d3804643907de7a49fd8fba745918ad99f7606b7c42d073fdd5b950e986fa4251e99be4ba9a5bf879a030b57b2e4085ac5d07a40028b08f26ed94acc'
